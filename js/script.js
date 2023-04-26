a03e65991e18585be54d619fd9370b257d4062a2
Dharmendra Yadav <114725125+DharmendraYaddav@users.noreply.github.com>
2023-04-26 19:33:28 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,6 +5,10 @@ let feh=document.getElementById("inp2");
 cel.addEventListener('input',function(){
     let c=this.value;
     let f=(c* 9/5) + 32;
+    if(!Number.isInteger(f)){
+        f=f.toFixed(4);
+
+    }
     feh.value=f;
     
 })
@@ -12,5 +16,8 @@ cel.addEventListener('input',function(){
 feh.addEventListener('input',function(){
    let f=this.value;
    let c=(f - 32) * 5/9;
+   if(!Number.isInteger(f)){
+    c=c.toFixed(4);
+   }
    cel.value=c;
 })
